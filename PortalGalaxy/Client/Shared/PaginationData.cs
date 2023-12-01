f7b06466e85ae481ebf80811ff91bcc836ce5341
Erick Orlando <erickorlando@outlook.com>
2023-11-30 22:08:39 -0500

    Paginador Blazor

---
+++ PortalGalaxy/Client/Shared/PaginationData.cs
@@ -0,0 +1,14 @@
+﻿namespace PortalGalaxy.Client.Shared;
+
+public class PaginationData
+{
+    public int CurrentPage { get; set; }
+    public int RowPerPage { get; set; }
+    public int TotalPages { get; set; }
+    public int RowCount { get; set; }
+}
+
+public class PagedResult<T> : PaginationData
+{
+    public ICollection<T> Results { get; set; } = new List<T>();
+}
