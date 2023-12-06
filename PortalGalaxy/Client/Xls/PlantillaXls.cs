b85338ebc1d13b7cdf5dcacc37edc9a7b4900a9f
Erick Orlando <erickorlando@outlook.com>
2023-12-06 14:59:42 -0500

    Exportacion a Excel a traves de ClosedXML

---
+++ PortalGalaxy/Client/Xls/PlantillaXls.cs
@@ -0,0 +1,20 @@
+﻿using ClosedXML.Report;
+using PortalGalaxy.Shared.Response;
+
+namespace PortalGalaxy.Client.Xls;
+
+public class PlantillaXls
+{
+    public byte[] GenerarPlantillaTaller(Stream plantilla, ICollection<TallerDtoResponse> data)
+    {
+        var template = new XLTemplate(plantilla);
+        
+        template.AddVariable("Talleres", data);
+        template.Generate();
+        
+        using var ms = new MemoryStream();
+        template.SaveAs(ms);
+        
+        return ms.ToArray();
+    }
+}
